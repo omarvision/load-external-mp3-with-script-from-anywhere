b6b31a48c9163cbb25a4e87b72e33ee1f1f0e7e7
Omar Santiago <omarkenosia@gmail.com>
2021-07-11 12:49:44 -0400

    Add files via upload

---
+++ Song.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using UnityEngine;
+
+// load audio from file: https://www.mirimad.com/unity-load-audio-clip-from-file-or-url/
+
+[RequireComponent(typeof(AudioSource))]
+public class Song : MonoBehaviour
+{
+    private AudioSource aud = null;
+
+    private void Start()
+    {
+        aud = this.GetComponent<AudioSource>();
+        StartCoroutine(LoadAudio());
+    }
+    private IEnumerator LoadAudio()
+    {
+        string fullpath = "file:///" + "C:/Omar/song.wav";
+        WWW url = new WWW(fullpath);
+        yield return url;
+
+        aud.clip = url.GetAudioClip(false, true);
+        aud.Play();
+    }
+}
---
+++ SongNAudio.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class SongNAudio : MonoBehaviour
+{
+    public string path = "c:/omar/song.mp3";
+    private AudioSource aud = null;
+
+    private void Start()
+    {
+        aud = this.GetComponent<AudioSource>();
+        StartCoroutine(LoadSongCoroutine());
+    }
+    private IEnumerator LoadSongCoroutine()
+    {
+        string url = string.Format("file://{0}", path);
+        WWW www = new WWW(url);
+        yield return www;
+
+        aud.clip = NAudioPlayer.FromMp3Data(www.bytes);
+        aud.Play();
+    }
+}
